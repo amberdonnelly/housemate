b7e2d6eded8074ca0d059b6ac753fab6e83fc890
Cindy Di Xin Zhang <cindydxz@gmail.com>
2018-01-14 10:26:05 -0800

    added mongodb

--- housemateapp/server.js
+++ housemateapp/server.js
@@ -2,9 +2,16 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
+//database stuff
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/housemateapp');
+
 
 router.use(function (req,res,next) {
 //   console.log("/" + req.method);
+  //lets router access db
+  req.db = db;
   next();
 });
 
@@ -29,6 +36,23 @@ app.use("/",router);
 // app.use("*",function(req,res){
 //   res.sendFile(path + "404.html");
 // });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(3000,function(){
   console.log("Live at Port 3000");
